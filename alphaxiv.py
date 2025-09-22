ff300f8879a14ad6c68dbb5d04516956fb4100df
“yoshiyuki-miyamori” <timemachiiiine@gmail.com>
2025-09-22 11:40:31 +0900

    技術的負債を返済

--- alphaxiv.py
+++ alphaxiv.py
@@ -1,8 +1,9 @@
 DISCORD_TOKEN = "自分のDiscordのBotのトークンを貼り付ける"
-CHANNEL_ID = 自分のチャンネルIDを貼り付ける  # 通知を送信したいDiscordチャンネルのID
-ARXIV_CATEGORY = "stat.TH"  # チェックするarXivのカテゴリ = statistics theory
-CHECK_START_HOUR = 9 # チェックを開始する時刻
-CHECK_END_HOUR = 15 # チェックを終了する時刻
+CHANNEL_ID = 0000000000000000000 # 通知を送信したい自分のDiscordチャンネルのIDを貼り付ける
+ARXIV_CATEGORY = "stat.TH" # statistics theory
+MAX_RESULTS = 50
+CHECK_START_HOUR = 9
+CHECK_END_HOUR = 15
 
 import discord
 import requests
@@ -11,61 +12,57 @@ import datetime
 import asyncio
 import os
 
-ARXIV_API_URL = f"http://export.arxiv.org/api/query?search_query=cat:{ARXIV_CATEGORY}&sortBy=submittedDate&sortOrder=descending&max_results=50"
+ARXIV_API_URL = f"http://export.arxiv.org/api/query?search_query=cat:{ARXIV_CATEGORY}&sortBy=submittedDate&sortOrder=descending&max_results={MAX_RESULTS}"
 bot = discord.Client(intents=discord.Intents.default())
-JST = datetime.timezone(datetime.timedelta(hours=9)) # 日本時間
+Japan_Standard_Time = datetime.timezone(datetime.timedelta(hours=9))
 
-# チェック済みリストがまだない時は現在の論文リストとして作成
-if not os.path.exists("seen_papers.txt"):
-    with open("seen_papers.txt", "w") as seen_papers:
-        for entry in ET.fromstring(requests.get(ARXIV_API_URL).content).findall("{http://www.w3.org/2005/Atom}entry"):
-            paper_id = entry.find("{http://www.w3.org/2005/Atom}id").text.split("/abs/")[-1].split('v')[0]
-            seen_papers.write(f"{paper_id}\n")
+def Current_Papers():
+    current_papers = []
+    for entry in ET.fromstring(requests.get(ARXIV_API_URL).content).findall("{http://www.w3.org/2005/Atom}entry"):
+        title = entry.find("{http://www.w3.org/2005/Atom}title").text.strip()
+        paper_id, version = entry.find("{http://www.w3.org/2005/Atom}id").text.split("/abs/")[-1].split('v')
+        if version == '1':
+            current_papers.append((title, paper_id))
+    return current_papers
 
-# Botの動作
+def Seen_Papers():
+    if not os.path.exists("seen_papers.txt"):
+        with open("seen_papers.txt", "w") as seen_papers:
+            current_v1_paper_ids = [paper_id for _, paper_id in Current_Papers()]
+            seen_papers.writelines(f"{paper_id}\n" for paper_id in current_v1_paper_ids)
+    with open("seen_papers.txt", "r") as seen_papers:
+        seen_paper_ids = {paper_id.strip() for paper_id in seen_papers}
+    return seen_paper_ids
+
 @bot.event
 async def on_ready():
     print("arXivの更新を待っています...")
     while not bot.is_closed():
-        now = datetime.datetime.now(JST)
+        now = datetime.datetime.now(Japan_Standard_Time)
         start_time = now.replace(hour=CHECK_START_HOUR, minute=0, second=0, microsecond=0)
         end_time = now.replace(hour=CHECK_END_HOUR, minute=0, second=0, microsecond=0)
+        weekend = now.weekday() in [5, 6]
 
-        # 平日かつ活動時間内のみチェック
-        if now.weekday() not in [5, 6] and start_time <= now <= end_time:
-            new_papers_found = False
+        if not weekend and start_time <= now <= end_time:
             try:
-                with open("seen_papers.txt", "r+") as seen_papers:
-                    seen_paper_ids = {line.strip() for line in seen_papers}
-                    new_papers = []
-                    current_papers = set()
+                current_papers = Current_Papers()
+                seen_paper_ids = Seen_Papers()
+                new_papers = []
 
-                # arXiv APIから論文ID(version番号なし)を取得
-                for entry in ET.fromstring(requests.get(ARXIV_API_URL).content).findall("{http://www.w3.org/2005/Atom}entry"):
-                    paper_id = entry.find("{http://www.w3.org/2005/Atom}id").text.split("/abs/")[-1].split('v')[0]
-                    current_papers.add(paper_id) # current_papersに記録
-
-                    # チェック済みか確認
+                for title, paper_id in current_papers:
                     if paper_id not in seen_paper_ids:
-                        new_papers.append(entry) # new_papersに記録
-                        new_papers_found = True
+                        new_papers.append((title, paper_id))
 
-                # 新着論文が見つかった場合はseen_papers.txtを全部書き直す
-                if new_papers_found:
-                    seen_papers.seek(0); seen_papers.truncate() # 現在の中身を削除
-                    seen_papers.writelines(f"{paper_id}\n" for paper_id in current_papers) # current_papersの中身をコピー
+                if new_papers:
+                    os.remove("seen_papers.txt")
 
-                # new_papersのタイトルとURLを送信
-                for entry in new_papers:
-                    title = entry.find("{http://www.w3.org/2005/Atom}title").text.strip()
-                    paper_id = entry.find("{http://www.w3.org/2005/Atom}id").text.split("/abs/")[-1].split('v')[0]
-                    overview_url = f"https://www.alphaxiv.org/ja/overview/{paper_id}"
-                    await bot.get_channel(CHANNEL_ID).send(embed=discord.Embed(title=title, description=f"<{overview_url}>") )
+                for title, paper_id in new_papers:
+                    alphaXiv_URL = f"https://www.alphaxiv.org/ja/overview/{paper_id}"
+                    await bot.get_channel(CHANNEL_ID).send(embed=discord.Embed(title=title, description=f"<{alphaXiv_URL}>") )
 
             except Exception as e:
-                print(f"エラー: {e}")
+                print(f"Error: {e}")
 
-        # 10分後に再確認．無駄に思えるが，長時間スリープさせるとDiscordとの接続が不安定になる．
-        await asyncio.sleep(600)
+        await asyncio.sleep(600) # 長時間スリープさせるとDiscordとの接続が不安定になる
 
 bot.run(DISCORD_TOKEN)
